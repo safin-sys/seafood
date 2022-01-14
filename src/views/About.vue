1fdd18d3b97d1572d815e9c86e5e7b45d7dc9141
safin-sys <mdsafinofficial@gmail.com>
2022-01-15 02:39:05 +0600

    about, svg

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid place-content-center overflow-hidden h-[80vh]">
+    <div class="flex flex-col justify-center items-center overflow-hidden h-[80vh] px-4">
         <iframe
             width="360"
             height="202"
@@ -9,5 +9,6 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
         ></iframe>
+        <p class="max-w-[360px] mt-8 italic">&quot;Question for you. What's better than octopus recipe? Answer for you. Eight recipes for octopus.&quot; <br> - <span class="font-bold">Jian-Yang</span></p>
     </div>
 </template>
